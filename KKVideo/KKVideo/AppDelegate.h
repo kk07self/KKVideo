cba854f068bb7d383137a22c6c7eca3fd6ed60cb
陈康 <kk.07.self@gmail.com>
2019-07-15 19:15:52 +0800

    GPUImage小试牛刀

---
+++ KKVideo/KKVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKVideo
+//
+//  Created by tutu on 2019/7/15.
+//  Copyright © 2019 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKVideo/KKVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKVideo
+//
+//  Created by tutu on 2019/7/15.
+//  Copyright © 2019 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
